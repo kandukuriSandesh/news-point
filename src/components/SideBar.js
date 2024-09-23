ab7bc2685b2981a10dd6648c725d46592082b322
kandukuri Sandesh <kandukurisandesh1998@gmail.com>
2024-09-23 01:53:45 +0100

    changed one api

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,13 +7,13 @@ export default function SideBar(props) {
     <div className='my-3 SideBar' style={{width:"15%",height:"100%",float:"left"}}>
       
         <h1 style={{textAlign:"center"}} ><span className="badge badge-secondary" style={{backgroundColor:"blue",color:"white"}} >News Topics</span></h1>
-        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88`,"Latest")} className={props.active==="Latest"?"selected":""} > Latest News </h3>
-        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&q=social`,"Social")}  className={props.active==="Social"?"selected":""} >Social News</h3>
-        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&category=science`,"Science")}  className={props.active==="Science"?"selected":""}>Science</h3>
-        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&q=business`,"Business")}  className={props.active==="Business"?"selected":""}>Business</h3>
+        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88`,"Latest")} className={props.active==="latest"?"selected":""} > Latest News </h3>
+        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&q=social`,"Social")}  className={props.active==="social"?"selected":""} >Social News</h3>
+        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&category=science`,"Science")}  className={props.active==="science"?"selected":""}>Science</h3>
+        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&q=business`,"Business")}  className={props.active==="business"?"selected":""}>Business</h3>
 
         
-        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/sources?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88`,"News Source")} className={props.active==="News Source"?"selected":""} > News Source </h3>
+        <h3 onClick={()=> props.getAndInvoke(`https://newsdata.io/api/1/latest?apikey=pub_540957621ed02d46dc48df0806ef9405e4e88&q=health`,"Health")} className={props.active==="health"?"selected":""} > Health </h3>
 
     </div>
   )
